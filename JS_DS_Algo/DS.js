2985448a18247e36caf325ca35e69f5b8903683b
rajneeshdubey <rajneeshdubey@localhost>
2020-04-26 12:46:07 +0530

    DS in JS

---
+++ JS_DS_Algo/DS.js
@@ -0,0 +1,109 @@
+var head = null;
+
+function findBiggest(array){
+	let k = 0, len = array.length;
+	for(let i=0;i < len;i++){
+		if(array[k] < array[i]){
+			k = i;
+		}
+	}
+	console.log("Biggest Number is ", array[k]);
+}
+
+
+function fibo(N){
+	if(N==0 || N===1){
+		return N;
+	}else{
+		var result = fibo(N-1) + fibo(N-2);
+		return result;
+	}
+}
+
+function fiboWithoutRecurssion(N){
+	var last = 1, lastToLast = 01, summation = 01;
+	if(N == 0){
+		return 0;
+	}else if(N ==1){
+		return summation;
+	}else if(N==2){
+		return summation;
+	}
+	for(let i=3; i <= N; i++){
+		summation = last + lastToLast;
+		
+		lastToLast = last;
+		
+		last = summation;
+	}
+	return summation;
+}
+
+module.exports = {findBiggest : findBiggest, insertToLinkedList : insertToLinkedList,
+		printLinkedList : printLinkedList, getHeadOfLinkedList: function(){return head;}, insertToLinkedListAtLast,
+		countLinkedListItems, fiboWithoutRecurssion, fibo, formCircularLinkedList, printKNodesOfLinkedList
+		
+};
+
+
+
+
+
+function insertToLinkedList(arg){
+	arg.next = head;
+	head = arg;
+}
+
+function insertToLinkedListAtLast(arg){
+	if(!head){
+	  head = arg;
+	  head.next = null;
+	  return;
+	}
+	var temp = head;
+	while(temp.next){
+		temp = temp.next;
+	}
+	temp.next = arg;
+	arg.next = null;
+}
+
+function printLinkedList(){
+	while(head){
+		console.log(head.data);
+		head = head.next;
+	}
+}
+
+function countLinkedListItems(){
+	var count = 0, temp = head;
+	while(temp){
+		temp = temp.next;
+		count++;
+	}
+	return count;
+}
+
+
+function formCircularLinkedList(N){
+  var head = {data:1, next:null};
+  var temp = head;
+  for(let i = 2 ;i<=N; i++){
+	  var newNode = {data:i, next:null};
+	  temp.next = newNode;
+	  temp = newNode;
+  }
+  temp.next = head;
+  //console.log(head);
+  return head;
+}
+
+function printKNodesOfLinkedList(head, K){
+	var temp = head;
+	for(let i=0;i<K && temp ;i++){
+		console.log(temp.data);
+		temp = temp.next;
+	}
+	return temp;
+}
+
---
+++ JS_DS_Algo/index.js
@@ -0,0 +1,21 @@
+var DS = require('./DS');
+
+DS.findBiggest([81, 1,2,43,4, 80]);
+
+
+/*DS.insertToLinkedList({data:1});
+DS.insertToLinkedList({data:11});
+DS.insertToLinkedList({data:21});
+DS.insertToLinkedListAtLast({data:100});
+DS.insertToLinkedList({data:201});
+console.log("Count-->>",DS.countLinkedListItems());
+DS.printLinkedList();*/
+
+console.log(DS.fiboWithoutRecurssion(11), "<<-- FIBO w/o Recursion");
+
+console.log(DS.fibo(11), "<<-- FIBO");
+
+var head = DS.formCircularLinkedList(5);
+var lasttolast = DS.printKNodesOfLinkedList(head, 7);
+
+console.log(lasttolast.next.next.data, "lTl");
