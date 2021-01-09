1613b0a623132699f1fd64376e2ed69c091bfd2e
Ishank Dubey <ishankdubey@gmail.com>
2021-01-10 00:14:28 +0530

    Adding DP Problems - Partion Problem, Finding sets that have a Sum K, General DP beginner problems

--- JS_DS_Algo/DP.js
+++ JS_DS_Algo/DP.js
@@ -82,13 +82,80 @@ function DP(){
 			setsWithSumOfKInner(sum, ini-1, index);
 		}
 	}//O(2^n), other way is find all the combinations and sum them
-    function print(array, index){
+	function print(array, index){
     	let newArray = [];
-    	for(let i=0;i < index;i++){
-    		newArray.push(array[i]);
-    	}
+	    	for(let i=0;i < index;i++){
+	    		newArray.push(array[i]);
+	    	}
     	console.log(newArray);
     }	
+	
+	/*
+	 * Partition Problem
+	 * Given an array of positive numbers - To check if the array(set) can be divided into
+	 * Two halves so that their sums are equal
+	 * 1. If the sum is odd - this is not possible to have two equal halves
+	 * 2. If the Sum is even the problem boils down to finding a sub set that sums upto sum/2
+	 * 3. This has a recurssion and a DP solution  
+	 * */
+	function recursivePartitionProblem(array, SUM){
+		if(SUM % 2 !=0 ){
+			return false;
+		}
+		return recurcivePartitioinProblemRecurssiveInner(array.length - 1, SUM/2);
+		function recurcivePartitioinProblemRecurssiveInner(ini, sum){
+			if(sum == 0){
+				return true;
+			}
+			if(ini < 0){
+				return false;
+			}
+			return (recurcivePartitioinProblemRecurssiveInner(ini - 1, sum - array[ini]) || 
+			  recurcivePartitioinProblemRecurssiveInner(ini -1, sum));
+		}
+	}//time complexity is O(2^n)
+	
+	/*
+	 * Partition problem described above via Dynamic Programming
+	 * 1. Lets maintain a 2-D array that has Sum as row and index as column
+	 * 2. We will create the 2-D array going Bottom-Up
+	 * 3. DP[s][n] represents that sum s is possible with sub-set of n elements
+	 * 4. Initial assumptions
+	 *    a. DP[0][0] = true, DP[0][x] = true since 0 can be 
+	 * */
+	function partitionProblemViaDP(array, SUM){
+		var DP = [];
+		
+		
+		
+		if(SUM % 2 != 0){
+			return false;
+		}
+		
+		for(let i=0;i <= SUM/2 ;i++){
+			DP.push([]);
+		}
+		
+		//SUM = SUM/2;
+		
+		for(let i=0;i <= array.length;i++){
+			DP[0][i] = true;
+		}
+		for(let j=1;j <= SUM/2;j++){
+			DP[j][0] = false;
+		}
+		
+		for(let i=1; i <= SUM/2 ;i++){
+			for(let j= 1; j <= array.length ;j++){
+				DP[i][j] = DP[i][j-1];
+				if(i >= array[j]){
+					DP[i][j] = DP[i][j] || DP[i - array[j]][j-1];
+				}
+			}
+		}
+		
+		return DP[SUM/2][array.length];
+	}//O(sum * n)
 	
 	return {
 		factorialRecurssion,
@@ -96,7 +163,9 @@ function DP(){
 		factorialWithDPAndIteration,
 		factorialWithDPAndRecurssion,
 		fibonacciWithDP,
-		setsWithSumOfK
+		setsWithSumOfK,
+		recursivePartitionProblem,
+		partitionProblemViaDP
 		};
 }
 module.exports = {DP}
