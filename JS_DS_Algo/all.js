6109fe540dcd6bf41a69d873004da4474af0a866
ishank dubey <ishankdubey@gmail.com>
2023-05-16 18:38:20 +0530

    matrix

--- JS_DS_Algo/all.js
+++ JS_DS_Algo/all.js
@@ -660,6 +660,28 @@ function All(){
 	   }
    }//printMatrixSpirally([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]], 4, 4)//Theta(R*C)
 
+   /*
+   * Start from the right most element in the first row
+   * if the givn element is lesset than redue the column
+   * * if the the given element is greater then move down wards
+   * *
+   * **/
+   function findElementInaRowandColumnSortedMatrix(matrix, R, C, item){
+	   let i = 0;
+	   let j = C - 1;
+	   while(i < R && j >=0 ){
+		   if(matrix[i][j]==item){
+			   console.log('found at', i, j);
+			   return;
+		   }else if(matrix[i][j] > item){
+                  j--;
+		   }else{
+			   i++;
+		   }
+	   }
+	   console.log('not found');
+   }//findElementInaRowandColumnSortedMatrix([[10,20,30,40],[15,25,35,45],[27,29,37,48],[32,33,39,50]], 4, 4, 39);
+
 
 	return {
 		increment,
