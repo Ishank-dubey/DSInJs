8fcf87d3dbe135ddd631a91c7eb408195924d653
ishank dubey <ishankdubey@gmail.com>
2026-06-16 20:50:09 +0530

    Update neetcode.js

--- JS_DS_Algo/neetcode.js
+++ JS_DS_Algo/neetcode.js
@@ -1,6 +1,39 @@
 function neetCode() {
 
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    checkValidString(s) {
+        const left = [];
+        const star = [];
+        for (let i = 0; i < s.length; i++) {
+            const ch = s[i];
+            if (ch === '(') {
+                left.push(i);
+            } else if (ch === '*') {
+                star.push(i);
+            } else {
+                if (left.length === 0 && star.length === 0) {
+                    return false;
+                }
+                if (left.length > 0) {
+                    left.pop();
+                } else {
+                    star.pop();
+                }
+            }
+        }
 
+        while (left.length > 0 && star.length > 0) {
+            if (left.pop() > star.pop()) return false;
+        }
+        return left.length === 0;
+    }
+}
+
+	
 function palindromeWithAlphaNumeric(str) {
     function isAlphaNumeric(char) {
         const code = char.charCodeAt(0);
