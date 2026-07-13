11d2eeec6f00e3928cbfc6a4328ca9ee02a636af
ishank dubey <ishankdubey@gmail.com>
2026-07-13 15:49:23 +0530

    Update neetcode.js

--- JS_DS_Algo/neetcode.js
+++ JS_DS_Algo/neetcode.js
@@ -1,5 +1,27 @@
 function neetCode() {
 
+	function mergeTwoSprtedLinledLists(head1, head2) {
+    let dummy = {data:-1, next:null};
+    let tail = dummy;
+    while(head1 && head2) {
+        if(head1.val < head2.val) {
+            tail.next = head1;
+            head1 = head1.next;
+        } else{
+            tail.next = head2;
+            head2 = head2.next;
+        }
+        tail = tail.next;
+    }
+    if (head1) {
+        tail.next = head1;
+    }
+    if (head2) {
+        tail.next = head2;
+    }
+    return dummy.next;
+}
+
 	function generateParanthesis(N) {
     let result = [];
     function DFS(open, close, expr) {
